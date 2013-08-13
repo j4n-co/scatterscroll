fcafd0dbd27a47156ff8d172692846cea6e2e518
Jan Drewniak <jan.drewniak@gmail.com>
2013-08-13 07:52:29 -0400

    normalizing mousewheel

--- scatterscroll.js
+++ scatterscroll.js
@@ -1,3 +1,128 @@
+/*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
+ * Licensed under the MIT License (LICENSE.txt).
+ *
+ * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
+ * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
+ * Thanks to: Seamus Leahy for adding deltaX and deltaY
+ *
+ * Version: 3.1.3
+ *
+ * Requires: 1.2.2+
+ */
+
+(function (factory) {
+    if ( typeof define === 'function' && define.amd ) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // Node/CommonJS style for Browserify
+        module.exports = factory;
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function ($) {
+
+    var toFix = ['wheel', 'mousewheel', 'DOMMouseScroll', 'MozMousePixelScroll'];
+    var toBind = 'onwheel' in document || document.documentMode >= 9 ? ['wheel'] : ['mousewheel', 'DomMouseScroll', 'MozMousePixelScroll'];
+    var lowestDelta, lowestDeltaXY;
+
+    if ( $.event.fixHooks ) {
+        for ( var i = toFix.length; i; ) {
+            $.event.fixHooks[ toFix[--i] ] = $.event.mouseHooks;
+        }
+    }
+
+    $.event.special.mousewheel = {
+        setup: function() {
+            if ( this.addEventListener ) {
+                for ( var i = toBind.length; i; ) {
+                    this.addEventListener( toBind[--i], handler, false );
+                }
+            } else {
+                this.onmousewheel = handler;
+            }
+        },
+
+        teardown: function() {
+            if ( this.removeEventListener ) {
+                for ( var i = toBind.length; i; ) {
+                    this.removeEventListener( toBind[--i], handler, false );
+                }
+            } else {
+                this.onmousewheel = null;
+            }
+        }
+    };
+
+    $.fn.extend({
+        mousewheel: function(fn) {
+            return fn ? this.bind("mousewheel", fn) : this.trigger("mousewheel");
+        },
+
+        unmousewheel: function(fn) {
+            return this.unbind("mousewheel", fn);
+        }
+    });
+
+
+    function handler(event) {
+        var orgEvent = event || window.event,
+            args = [].slice.call(arguments, 1),
+            delta = 0,
+            deltaX = 0,
+            deltaY = 0,
+            absDelta = 0,
+            absDeltaXY = 0,
+            fn;
+        event = $.event.fix(orgEvent);
+        event.type = "mousewheel";
+
+        // Old school scrollwheel delta
+        if ( orgEvent.wheelDelta ) { delta = orgEvent.wheelDelta; }
+        if ( orgEvent.detail )     { delta = orgEvent.detail * -1; }
+
+        // New school wheel delta (wheel event)
+        if ( orgEvent.deltaY ) {
+            deltaY = orgEvent.deltaY * -1;
+            delta  = deltaY;
+        }
+        if ( orgEvent.deltaX ) {
+            deltaX = orgEvent.deltaX;
+            delta  = deltaX * -1;
+        }
+
+        // Webkit
+        if ( orgEvent.wheelDeltaY !== undefined ) { deltaY = orgEvent.wheelDeltaY; }
+        if ( orgEvent.wheelDeltaX !== undefined ) { deltaX = orgEvent.wheelDeltaX * -1; }
+
+        // Look for lowest delta to normalize the delta values
+        absDelta = Math.abs(delta);
+        if ( !lowestDelta || absDelta < lowestDelta ) { lowestDelta = absDelta; }
+        absDeltaXY = Math.max(Math.abs(deltaY), Math.abs(deltaX));
+        if ( !lowestDeltaXY || absDeltaXY < lowestDeltaXY ) { lowestDeltaXY = absDeltaXY; }
+
+        // Get a whole value for the deltas
+        fn = delta > 0 ? 'floor' : 'ceil';
+        delta  = Math[fn](delta / lowestDelta);
+        deltaX = Math[fn](deltaX / lowestDeltaXY);
+        deltaY = Math[fn](deltaY / lowestDeltaXY);
+
+        // Add event and delta to the front of the arguments
+        args.unshift(event, delta, deltaX, deltaY);
+
+        return ($.event.dispatch || $.event.handle).apply(this, args);
+    }
+
+}));
+
+
+
+
+
+
+
+
 $(document).ready(function(){
   
 /*------- get the DOM elems -------*/
@@ -48,7 +173,7 @@ function scatter(a){
     $('#posts').css({'height':a.outerHeight() + docH*0.3,'position':'absolute'})
     window.setTimeout(function(){
       bindMouseScroll()      
-    }, 500)
+    }, 50)
   }    
   a.bottomSwipeOnReverseComplete = function(){
     unbindMouseScroll()
@@ -58,7 +183,7 @@ function scatter(a){
     
     window.setTimeout(function(){
       bindMouseScroll()      
-    }, 500)  
+    }, 50)  
   } 
 
   /* top animation */
@@ -69,7 +194,7 @@ function scatter(a){
  
     window.setTimeout(function(){
       bindMouseScroll()
-    }, 700)
+    }, 50)
   }
   a.topSwipeOnReverseComplete = function(){
     unbindMouseScroll()    
@@ -81,7 +206,7 @@ function scatter(a){
 
     window.setTimeout(function(){
       bindMouseScroll()      
-    }, 700)
+    }, 50)
   }
 
   a.topSwipeOnStart = function(){
@@ -97,14 +222,14 @@ function scatter(a){
  
     window.setTimeout(function(){
       bindMouseScroll()
-    }, 700)   
+    }, 50)   
   }
 
   a.bottomSwipe = new TimelineMax({paused:true})  
-  a.bottomSwipe.to(a, 0.5, { top:$('#posts').offset().top, rotation: 0, transformOrigin:"0 50%", left: doc.width()/2 - a.width()/2 , onStart: a.bottomSwipeOnStart, onComplete: a.bottomSwipeOnComplete, onReverseComplete: a.bottomSwipeOnReverseComplete })
+  a.bottomSwipe.to(a, 0.3, { top:$('#posts').offset().top, rotation: 0, transformOrigin:"0 50%", left: doc.width()/2 - a.width()/2 , onStart: a.bottomSwipeOnStart, onComplete: a.bottomSwipeOnComplete, onReverseComplete: a.bottomSwipeOnReverseComplete })
 
   a.topSwipe = new TimelineMax({paused: true})
-  a.topSwipe.to(a, 0.5, { bottom: docH-300 ,rotation: a.deg ,transformOrigin:"100% 50%",  onStart: a.topSwipeOnStart, onComplete: a.topSwipeOnComplete, onReverseComplete: a.topSwipeOnReverseComplete })
+  a.topSwipe.to(a, 0.3, { bottom: docH-200,rotation: a.deg ,transformOrigin:"100% 50%",  onStart: a.topSwipeOnStart, onComplete: a.topSwipeOnComplete, onReverseComplete: a.topSwipeOnReverseComplete })
 
 }
 
@@ -116,7 +241,7 @@ $.each(items, function( index , item){
 /*------- mousewheel handler -------*/  
 var item;
 var itemIndex; 
-function MouseWheelHandler(event){
+function MouseWheelHandler(event, delta, deltaX, deltaY){
   
   if (!item){
     itemIndex = 0
@@ -142,6 +267,8 @@ function MouseWheelHandler(event){
         item.topSwipe.play()
         if ((item.outerHeight() - container.outerHeight() -  container.scrollTop()) > 10){
           item.state = 'inFocus'
+          //items[itemIndex-1].state = 'atTop'
+          //items[itemIndex+1].state = 'atBottom'
         } 
       break;
       
@@ -171,6 +298,15 @@ function MouseWheelHandler(event){
           item.topSwipe.reverse()
           item.onTopStart()
       break;
+
+      case 'doneScrolling':
+        item.topSwipe.play()
+        if ((item.outerHeight() - container.outerHeight() -  container.scrollTop()) > 10){
+          item.state = 'inFocus'
+          items[itemIndex-1].state = 'atTop'
+          items[itemIndex+1].state = 'atBottom'
+        } 
+      break;
     }    
   }
   return false
@@ -179,38 +315,16 @@ function MouseWheelHandler(event){
 
 //binding
 function bindMouseScroll(){
-  //doc.bind('mousewheel', MouseWheelHandler)
-  
-  if (doc[0].addEventListener) {
-      doc[0].addEventListener("mousewheel", MouseWheelHandler, false)
-      doc[0].addEventListener("DOMMouseScroll", MouseWheelHandler, false)
-    }
-  else {
-      doc[0].attachEvent("onmousewheel", MouseWheelHandler)
-  }
-  
-  //console.log('binding')
-  //console.log(doc.data('events'))
-
+  $(window).bind("mousewheel", function(event, delta, deltaX, deltaY){
+    MouseWheelHandler(event, delta, deltaX, deltaY)
+  } )
 }
 
 bindMouseScroll();
 
 
 function unbindMouseScroll(){
-  //alert('unbinding happening')
-  
-  if (doc[0].removeEventListener) {
-      doc[0].removeEventListener("mousewheel", MouseWheelHandler, false)
-      doc[0].removeEventListener("DOMMouseScroll", MouseWheelHandler, false)
-    }
-  else {
-      doc[0].detachEvent("onmousewheel", MouseWheelHandler)
-  }
-  
-   // console.log('unbinding')
-  //doc.unbind('mousewheel', MouseWheelHandler)
-  //console.log(doc.data('events'))
+  $(window).unbind("mousewheel")
 }
 
 })
